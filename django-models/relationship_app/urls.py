b87e8f94dd42ac1e0b9f5dc08af4bbadf6b6b66b
KaleabTm <kaltamiru8@gmail.com>
2024-11-08 22:20:14 +0300

    login

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, UserRegisterView, UserLoginView, UserLogoutView
+from .views import list_books, LibraryDetailView, UserRegisterView, login_view, UserLogoutView
 
 url_patterns = [
     path('booklist/',list_books),
     path('librarydetail/', LibraryDetailView.as_view()),
     path('register/', UserRegisterView.as_view()),
-    path('login/', UserLoginView.as_view()),
+    path('login/', login_view),
     path('logout/', UserLogoutView.as_view()),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 # Create your views here.
 def list_books(request):
@@ -26,9 +28,20 @@ class UserRegisterView(CreateView):
     template_name = 'relationship_app/register.html'
     success_url = '/login/'
 
-class UserLoginView(LoginView):
+# class UserLoginView(LoginView):
+#     form = AuthenticationForm()
+#     template_name = 'relationship_app/login.html'
+
+
+def login_view(request):
     form = AuthenticationForm()
+    user = (request, form.get_user())
     template_name = 'relationship_app/login.html'
+    if user is not None:
+        login(request, user)
+    else:
+        ...
 
 class UserLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+    logout()
