49cf9a2004d1abd9bcf0a57f73d2ea0af55d8caa
KaleabTm <kaltamiru8@gmail.com>
2024-11-09 00:30:23 +0300

    urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import list_books, LibraryDetailView, register, LoginView, LogoutView, member, librarian
 from .views import *
 from .admin_view import admin_view
+from .member_view import member_view
+from .librarian_view import librarian_view
 
 
 url_patterns = [
@@ -9,8 +11,8 @@ url_patterns = [
     path('librarydetail/', LibraryDetailView.as_view()),
     path('register/', register),
     path('admin/', admin_view),
-    path('member/', member),
-    path('librarian/',librarian),
+    path('member/', member_view),
+    path('librarian/',librarian_view),
     path('login/', LoginView.as_view(template_name="relationship_app/login.html")),
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html")),
 ]
