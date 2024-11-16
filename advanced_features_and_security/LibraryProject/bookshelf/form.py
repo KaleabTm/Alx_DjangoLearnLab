62bcde5364c4d888e0e935ced6bdd4d4cd5a3bb5
KaleabTm <kaltamiru8@gmail.com>
2024-11-17 01:53:44 +0300

    ExampleForm

--- advanced_features_and_security/LibraryProject/bookshelf/form.py
+++ advanced_features_and_security/LibraryProject/bookshelf/form.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Book  # Import the Book model
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']  # Include the fields you want to capture
