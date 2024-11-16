4a809a5d821446cf45089b23f79cc5e9c3b0eb55
KaleabTm <kaltamiru8@gmail.com>
2024-11-17 02:00:04 +0300

    views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import permission_required
+
+from bookshelf.form import ExampleForm
 from .models import Book
 # Create your views here.
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        book = Book.objects.create(title=title, author=author)
-        book.save()
-    return redirect('book_list')
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request,id):
@@ -33,3 +36,6 @@ def edit_book(request):
 def view_book(request):
     book = get_object_or_404(Book, pk=id)
     return render("you have success fully retrieved a book")
+
+
+
