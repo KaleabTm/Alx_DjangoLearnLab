7af1d5734f7f8ea8f160a4ad2691ec6617412d3f
KaleabTm <kaltamiru8@gmail.com>
2024-11-22 01:00:00 +0300

    created a serializer, view and urls for the Book model

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 # Create your views here.
+
+
+class BookList(generics.ListAPIView):
+    serializer_class=BookSerializer
+    queryset=Book.objects.all()
