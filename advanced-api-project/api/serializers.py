b9f2f70e6d733c39a18fd8808449f4bc9b32a319
KaleabTm <kaltamiru8@gmail.com>
2024-11-29 14:56:36 +0300

    serializer validator updated

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -7,6 +7,10 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+    def validate(self, data):
+        if len(data['title']) < 5:
+            raise serializers.ValidationError("Title must be at least 5 characters long.")
+        return data
 
 
 # In this serializer we are seializing Auther and the related books that are written by him/her is included too
