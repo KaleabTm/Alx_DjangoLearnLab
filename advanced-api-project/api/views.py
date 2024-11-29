00df55cfdc5385a2b0435467eced9066ea57b0c3
KaleabTm <kaltamiru8@gmail.com>
2024-11-29 15:22:49 +0300

    permission

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from rest_framework import status, generics
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import mixins
@@ -9,9 +9,10 @@ class BookListView(mixins.ListModelMixin,generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookCreateView(generics.CreateAPIView):
+class BookCreateView(mixins.CreateModelMixin,generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BookDetailView(generics.RetrieveAPIView):
@@ -21,9 +22,11 @@ class BookDetailView(generics.RetrieveAPIView):
 class BookDeleteView(generics.RetrieveDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 class BookUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
     
