afe938772b039838ef69e6b719fc2151b353e145
KaleabTm <kaltamiru8@gmail.com>
2024-12-05 14:01:49 +0300

    setting up templates, form and views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView
+from .forms import UserCreatetion
+from django.contrib.auth.views import LoginView, LogoutView 
+from django.contrib.auth import login
 
 # Create your views here.
+class Login(LoginView):
+    template_name='login.html'
+
+class Logout(LogoutView):
+    template_name='logout.html'
+
+
+class UserRegister(CreateView):
+    form_class = UserCreatetion  
+    template_name = 'registration.html'  
+    success_url = '/profile/'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.success_url)
+  
+
+
+@login_required
+def profile(request):
+    return render(request, 'registration/profile.html', {'user': request.user})
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
 STATICFILES_DIR = [
     BASE_DIR / 'blog/static',
 ]
