624ca28619f41dd0ceea1efa70603f79e2ad8242
KaleabTm <kaltamiru8@gmail.com>
2024-12-05 14:37:27 +0300

    static file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,15 +7,15 @@ from django.contrib.auth import login
 
 # Create your views here.
 class Login(LoginView):
-    template_name='login.html'
+    template_name='registration/login.html'
 
 class Logout(LogoutView):
-    template_name='logout.html'
+    template_name='registration/logout.html'
 
 
 class UserRegister(CreateView):
     form_class = UserCreatetion  
-    template_name = 'register.html'  
+    template_name = 'registration/register.html'  
     success_url = '/profile/'
 
     def form_valid(self, form):
@@ -27,4 +27,4 @@ class UserRegister(CreateView):
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html', {'user': request.user})
+    return render(request, 'registration/profile.html', {'user': request.user})
