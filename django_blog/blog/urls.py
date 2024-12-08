9f45a2ec43e45b83ce428e1b437b72625c1ae5a3
KaleabTm <kaltamiru8@gmail.com>
2024-12-08 19:13:08 +0300

    url updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('posts/new/', PostCreateView.as_view(), name='post-create'),  
     path('posts/', PostListView.as_view(), name='post-list'),  
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  
-    path('/posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  
 ]
