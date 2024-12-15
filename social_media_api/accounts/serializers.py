582a2ab0fc8875431dcde083f0ba6b5a77c0faef
KaleabTm <kaltamiru8@gmail.com>
2024-12-15 23:12:45 +0300

    updated the serializer with Token

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,16 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'bio', 'profile_picture', 'followers']
-        read_only_fields = ['followers']  # Followers can't be modified directly
+        read_only_fields = ['id', 'followers']  # Make followers read-only
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     class Meta:
         model = get_user_model()
         fields = ['username', 'password', 'bio', 'profile_picture']
@@ -20,8 +22,10 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             password=validated_data.pop('password'),
             bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None),
+            profile_picture=validated_data.get('profile_picture', None)
         )
+        # Create a token for the new user
+        Token.objects.create(user=user)
         return user
 
 
