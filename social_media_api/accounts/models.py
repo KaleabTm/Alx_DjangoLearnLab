01fc5f0d036478758c9dea0bab9aeeaaa29d91a8
KaleabTm <kaltamiru8@gmail.com>
2024-12-15 23:04:41 +0300

    updated the model fields

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -8,8 +8,8 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=200)
     email = models.EmailField(unique=True, null=False, blank=False)
     password = models.CharField(max_length=200)
-    bio = models.CharField(max_length=200)
-    profile_picture = models.BinaryField(blank=True, null=True)
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_pics/',blank=True, null=True)
     followers = models.ManyToManyField('self',symmetrical=False)
     username=None
 
